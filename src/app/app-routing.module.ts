e097f7967e185ea7838a80654bec9c421cd950be
Yuri Bessa <yuri14.levita@gmail.com>
2022-08-31 01:46:22 -0300

    Terminando os detalhes da pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    loadChildren: () =>
+      import('./tela-home/home.module').then((m) => m.HomeModule),
   },
 ];
 
